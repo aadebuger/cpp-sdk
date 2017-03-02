75152f8003bb14e0c2b8e8fe29d468d2242e4f23
hua zhuang <zh@zqf.com.cn>
2017-03-02 19:58:44 +0800

    first commit

---
+++ src/Request/CurlClient.cpp
@@ -0,0 +1,9 @@
+//
+//  CurlClient.cpp
+//  AVOSCloud
+//
+//  Created by hua zhuang on 2/3/2017.
+//
+//
+
+#include "CurlClient.hpp"
---
+++ src/Request/CurlClient.hpp
@@ -0,0 +1,14 @@
+//
+//  CurlClient.hpp
+//  AVOSCloud
+//
+//  Created by hua zhuang on 2/3/2017.
+//
+//
+
+#ifndef CurlClient_hpp
+#define CurlClient_hpp
+
+#include <stdio.h>
+
+#endif /* CurlClient_hpp */
